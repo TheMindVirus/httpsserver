b6a8542f0c36b7858e1df95741d176e6c11cac59
Alastair Cota <a1.cota.365@gmail.com>
2022-04-06 23:56:43 +0100

    Prototyped RackSlashSpace
    Based on Reddit's r/place

--- server.py
+++ server.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python3
 import socket, ssl, http.server
+from PIL import Image
 
 # https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
 # sudo openssl req -newkey rsa:2048 -nodes -keyout domain.key -x509 -days 365 -out domain.crt
 
+CANVAS_FILE = "./web/canvas.png"
+
+def update_image(x, y, c):
+    x = int(x)
+    y = int(y)
+    c = int("0x" + str(c[1:]), 16)
+    r, g, b = [(c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF]
+    img = Image.open(CANVAS_FILE)
+    img.putpixel((x, y), (r, g, b))
+    img.save(CANVAS_FILE)
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory = "web", **kwargs)
+
+    def do_POST(self):
+        length = int(self.headers["Content-Length"])
+        data = self.rfile.read(length).decode("utf-8")
+        cmds = data.split("x")
+        if (len(cmds) == 3):
+            self.send_response(200)
+            self.wfile.write(data.encode("utf-8"))
+            print(cmds)
+            update_image(*cmds)
+        else:
+            self.send_response(500)
+            self.wfile.write("Bad Request")
 
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
@@ -19,3 +44,4 @@ httpd = http.server.HTTPServer(("0.0.0.0", 443), Handler)
 httpd.socket = ctx.wrap_socket(httpd.socket, server_hostname = "localhost")
 httpd.serve_forever()
 
+#update_image(*("500x500x#FF0000".split("x")))
